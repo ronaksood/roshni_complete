44b5cd26a822cdbe6d1b2c9a3fd5d90a59b4ff0d
fahamnaz <fahamchd@gmail.com>
2026-04-11 19:06:44 +0530

    backend inetgrated with frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5001",
+        target: "https://panipat-backend.onrender.com",
         changeOrigin: true,
       },
     },
