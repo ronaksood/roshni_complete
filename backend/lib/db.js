8eb4235482e6c8243612e2f25648834dff719b5d
ronaksood <soodronak04@gmail.com>
2026-04-13 13:16:11 +0530

    done

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
+// import dns from 'node:dns';
+// dns.setServers(['8.8.8.8', '1.1.1.1']);
+import 'dotenv/config';
 
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
+    console.log(error);
     console.log("Error connecting to MONGODB", error.message);
     process.exit(1);
   }
